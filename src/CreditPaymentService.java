fc9b04fa3f445146c2aaf1f7e13ae186c5ab3cb9
Ivlev Maksim <brainlucker@gmail.com>
2022-01-05 13:38:22 +0300

    Add comments

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,11 @@
 public class CreditPaymentService {
     public String calculate (int sum, int period, double rate) {
-        period *= 12;
-        rate /= 12 * 100;
-        double pow = Math.pow(1 + rate, period);
-        double multiplier = rate * pow / (pow - 1);
-        int result = (int) Math.round(sum * multiplier);
-        return String.format("%,d", result);
+        System.out.println("\nКредит на сумму " + String.format("%,d", sum) + " ₽ на " + period + " год(а) под " + rate + " % годовых.");
+        period *= 12; // Перевод срока в месяцы
+        rate /= 12 * 100; // Расчет ежемесячной процентной ставки
+        double power = Math.pow(1 + rate, period); // Расчет степени
+        double multiplier = rate * power / (power - 1); // Расчет коэффициента аннуитета
+        int result = (int) Math.round(sum * multiplier); // Расчет аннуитетного платежа с округлением до рублей
+        return String.format("%,d", result); // Возврат результата в виде строки с разделителем
     }
 }
